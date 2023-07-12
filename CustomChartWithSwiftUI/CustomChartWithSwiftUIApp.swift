56d8321c1fa89144be42ab12ca7bf353c2b75056
Sangdon Kim <107008333+sangdon-kim-switchwon@users.noreply.github.com>
2023-07-12 15:56:10 +0900

    Initial Commit

---
+++ CustomChartWithSwiftUI/CustomChartWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomChartWithSwiftUIApp.swift
+//  CustomChartWithSwiftUI
+//
+//  Created by PNX on 2023/07/12.
+//
+
+import SwiftUI
+
+@main
+struct CustomChartWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
