548d8f38b9d4263d3ea842c0eebebb02d95defd3
김상돈 <tkdehs0454@gmail.com>
2023-07-13 17:27:42 +0900

    개념 공부
    App, Scene, View 개녕 학습

--- CustomChartWithSwiftUI/ContentView.swift
+++ CustomChartWithSwiftUI/ContentView.swift
@@ -9,16 +9,30 @@ import SwiftUI
 import RxSwift
 
 struct ContentView: View {
+    
+    @State var data: String = "0"
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text(data)
                 .font(FONT_NOTOSANS_KR_BOLD(30))
+                .padding()
+            Button {
+                data = "\(Int.random(in: 1...100))"
+            } label : {
+                Text("Update")
+            }
                 
         }
         .padding()
+        .onAppear {
+            data = "0"
+        }.onDisappear {
+            data = "0"
+        }
     }
 }
 
@@ -27,3 +41,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
